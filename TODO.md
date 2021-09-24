32cc0c6a70942213b2b57fdb82c9578b789cb97f
Ole Kristian Sunde <ole.sunde@gmail.com>
2021-09-24 19:30:12 +0200

    Refactor (split) POST test

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 # TODO
 
-- Code smell: POST test is too long?
 - Display multiple items in the table
 - Support more than one list
+- ~~Code smell: POST test is too long?~~
 - ~~Don't save blank items for every request~~
--- lists/views.py
+++ lists/views.py
@@ -1,17 +1,13 @@
 from lists.models import Item
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
 
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
 
-    return render(request, 'home.html', {
-        'new_item_text': new_item_text,
-    })
+    return render(request, 'home.html')
