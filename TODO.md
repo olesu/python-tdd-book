d61d7e4dd61f35e4b2228db647c1aad5c51f9562
Ole Kristian Sunde <ole.sunde@gmail.com>
2021-09-24 19:11:53 +0200

    Save items

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO
+
+- Don't save blank items for every request
+- Code smell: POST test is too long?
+- Display multiple items in the table
+- Support more than one list
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,4 @@
+from lists.models import Item
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
@@ -5,6 +6,10 @@ from django.shortcuts import render
 
 
 def home_page(request):
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
+
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
+        'new_item_text': item.text,
     })
