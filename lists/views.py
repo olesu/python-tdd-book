a6954e79f638b0d7db64ae41dac77fc2768de32c
Ole Kristian Sunde <ole.sunde@gmail.com>
2021-09-24 19:16:07 +0200

    Only save items when necessary

--- lists/views.py
+++ lists/views.py
@@ -6,10 +6,12 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    item = Item()
-    item.text = request.POST.get('item_text', '')
-    item.save()
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
 
     return render(request, 'home.html', {
-        'new_item_text': item.text,
+        'new_item_text': new_item_text,
     })
