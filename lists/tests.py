e02d43ed1ec5a1ff60ee382e147b439d64351d10
Ole Kristian Sunde <ole.sunde@gmail.com>
2021-09-26 08:14:05 +0200

    Add list view template, refactoring

--- lists/tests.py
+++ lists/tests.py
@@ -35,6 +35,10 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
 
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
 
 class HomePageTest(TestCase):
 
