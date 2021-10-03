36abb60e4995f6270e2f5354c0c9c51da396badf
Ole Kristian Sunde <ole.sunde@gmail.com>
2021-10-03 17:24:16 +0200

    Fix typo: Put ALLOWED_HOSTS inside a list

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = os.environ['SITENAME']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
 else:
     DEBUG = True
     # SECURITY WARNING: keep the secret key used in production secret!
