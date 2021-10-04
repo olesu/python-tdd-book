a9cd873db47c9177d6b095bda4e72315fb710e79
Ole Kristian Sunde <ole.sunde@gmail.com>
2021-10-04 20:52:23 +0200

    fix typo

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -42,7 +42,7 @@ def _create_or_update_dotenv():
 
 
 def _update_static_files():
-    run('./virtualenv/bin/python manage.py collecstatic --noinput')
+    run('./virtualenv/bin/python manage.py collectstatic --noinput')
 
 
 def _update_database():
